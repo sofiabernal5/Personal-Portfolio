7286e1dd6566a9e38753058ead832d172d333ff1
Sofia Bernal <sb22i@fsu.edu>
2025-04-16 14:27:13 -0400

    imporving css and adding more functionality

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, abort
+import os
 
 app = Flask(__name__)
 
@@ -9,7 +10,25 @@ def home():
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html') 
+    return render_template('projects.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/download/<path:filename>')
+def download_file(filename):
+    try:
+        return send_from_directory(
+            os.path.join(app.root_path, 'static/files'),
+            filename, as_attachment=True
+        )
+    except FileNotFoundError:
+        abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
